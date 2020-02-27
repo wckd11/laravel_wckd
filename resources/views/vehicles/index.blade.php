0ce75fd74ab8d222a7ffe0767e9097642cdaf5a9
WCKDKeinthxz <wckdkeinthxz11@gmail.com>
2020-02-27 14:17:13 +0800

    laravel_wckd 1-5

---
+++ resources/views/vehicles/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <h1>Vehicles List Here</h1>
+            <table class="table">
+            	<thead>
+            		<tr>
+            			<th>#</th>
+            			<th>Name</th>
+            		</tr>
+            	</thead>
+            	<tbody>
+            		<tr>
+            			<td>1.</td>
+            			<td>Toyota Hilux</td>
+            		</tr>
+            		<tr>
+            			<td>2.</td>
+            			<td>Mitsubishi Montero</td>
+            		</tr>
+            		<tr>
+            			<td>3.</td>
+            			<td>Ford Raptor</td>
+            		</tr>
+            	</tbody>
+            </table>
+        </div>
+    </div>
+</div>
+@endsection
+
+
